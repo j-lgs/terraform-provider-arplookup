7e7e7c257fccb6ef2a49a673ddc8eb335b3fb6cc
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-06-13 19:56:08 +1000

    chore: prepare release

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         tpa = pkgs.buildGo118Module {
           inherit system;
           pname = "terraform-provider-arplookup";
-          version = "0.1.0";
+          version = "0.1.2";
 
           src = ./.;
 
